70fe54d1ef7b7bf454b0da6dcc2b610610d19dba
kevposesp <kevposesp@gmail.com>
2024-03-23 14:46:25 +0100

    type list shop

--- frontend/src/components/client/filters/Pagination.jsx
+++ frontend/src/components/client/filters/Pagination.jsx
@@ -43,17 +43,21 @@ const Pagination = ({ totalPages }) => {
     }
 
     return (
-        <nav className='mt-7 mb-3 flex'>
-            <ul class="inline-flex -space-x-px text-base h-10 mx-auto">
-                <li>
-                    <a class="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700" onClick={() => page > 1 ? handlePageChange(page - 1) : ''}>Previous</a>
-                </li>
-                {paginationButtons}
-                <li>
-                    <a class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700" onClick={() => page < totalPages / limit ? handlePageChange(page + 1) : ''}>Next</a>
-                </li>
-            </ul>
-        </nav>
+        <>
+            {paginationButtons.length > 0 && (
+                <nav className='mt-7 mb-3 flex'>
+                    <ul class="inline-flex -space-x-px text-base h-10 mx-auto">
+                        <li>
+                            <a class="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700" onClick={() => page > 1 ? handlePageChange(page - 1) : ''}>Previous</a>
+                        </li>
+                        {paginationButtons}
+                        <li>
+                            <a class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700" onClick={() => page < totalPages / limit ? handlePageChange(page + 1) : ''}>Next</a>
+                        </li>
+                    </ul>
+                </nav>
+            )}
+        </>
     );
 
 };
